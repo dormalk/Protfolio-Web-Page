50d05a6cfab375bfd2cbd13cff235f0f9fd22b61
Dormalk <dormalk@gmail.com>
2019-10-06 20:14:26 +0300

    Updates

---
+++ precache-manifest.f15c9bb4d4b8b2ac57eec11ece85eace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301009bed4e5cdffc35b3ca2f1789ad5",
+    "url": "/Protfolio-Web-Page/index.html"
+  },
+  {
+    "revision": "72a578273aa175a5ce6a",
+    "url": "/Protfolio-Web-Page/static/css/main.64364446.chunk.css"
+  },
+  {
+    "revision": "6a264dc120498050387d",
+    "url": "/Protfolio-Web-Page/static/js/2.186b54af.chunk.js"
+  },
+  {
+    "revision": "72a578273aa175a5ce6a",
+    "url": "/Protfolio-Web-Page/static/js/main.1fa6aced.chunk.js"
+  },
+  {
+    "revision": "b702301111f501d2454d",
+    "url": "/Protfolio-Web-Page/static/js/runtime-main.b41b7c90.js"
+  }
+]);
